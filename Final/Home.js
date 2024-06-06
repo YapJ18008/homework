f99a80e0ce2f5cda1b01644a1902ea45fdcf525a
YapJ18008 <155941299+YapJ18008@users.noreply.github.com>
2024-06-06 12:39:47 +0800

    1

--- Final/Home.js
+++ Final/Home.js
@@ -1,11 +1,11 @@
 const bodyElement = document.body;
-window.addEventListener("scroll", function (auto) {
+window.addEventListener("scroll", function () {
   const scrollTop = window.scrollY; // Vertical scroll position
   console.log(scrollTop);
 
   if (scrollTop > 49) {
-    bodyElement.classList.add("header-auto");
+    bodyElement.classList.add("header-fixed");
   } else {
-    bodyElement.classList.remove("header-auto");
+    bodyElement.classList.remove("header-fixed");
   }
 });
